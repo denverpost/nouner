c486ebb727a952f98531ab4adaf9e9d8b080121a
freejoe76 <jmurphy@denverpost.com>
2015-09-24 19:08:52 -0400

    Update include

--- www/include.js
+++ www/include.js
@@ -1,6 +1,13 @@
+<script>
 // The js to include on the preps article template.
 // Loads the lookup, searches for matches.
+// MATCHER
+var matcher_config = {
+    elements: '#articleBody p',
+    section: 'broncos'
+};
+
 var p;
-//p = "http://localhost/work/preps/site/";
-p = "http://extras.denverpost.com/app/preps/";
+p = "http://extras.denverpost.com/app/nouner/";
 $.getScript(p + "matcher.js", function() { matcher.init(); });
+</script>
