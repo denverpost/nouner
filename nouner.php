2d0a698ada52dff509c64dd57111346530b3b332
freejoe76 <jmurphy@denverpost.com>
2015-12-14 18:48:57 -0500

    Start on the WP plugin

---
+++ nouner.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Nouner, the Noun-Matcher
+ * Plugin URI: https://github.com/denverpost/nouner
+ * Description: Take a lookup table of proper nouns => URLs, and link any of the proper nouns to its corresponding URL in any given blog post.
+ * Version: 0.1
+ * Author: Joe Murphy
+ * Author URI: http://twitter.com/joemurph
+ * License: MIT
+ */
+
