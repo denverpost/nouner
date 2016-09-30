df47aa02235eea82f804bf05bd2b31de9f522f92
freejoe76 <jmurphy@denverpost.com>
2016-09-30 16:17:31 -0400

    Update matcher.js to accommodate other objects passed to it.

--- www/matcher.js
+++ www/matcher.js
@@ -18,8 +18,8 @@
 
 var matcher = {
     config: {
-        dir: 'http://extras.denverpost.com/app/nouner/lookup/',
-        elements: '#articleBody p, #articleBody td',
+        dir: '//extras.denverpost.com/app/nouner/lookup/',
+        elements: '',
         section: 'broncos'
     },
     lookup: {},
@@ -35,7 +35,8 @@ var matcher = {
     },
     regex: new RegExp(/\b([A-Z]\.?[A-Z]?\.?[a-z]*[A-Z]?[a-z]*)\s(([A-Z][a-z]+)\s?)+\b/gm),
     match: function() {
-        $(this.config.elements).each( function() { 
+        if ( typeof this.config.elements == 'string' ) this.config.elements = jQuery(this.config.elements);
+        this.config.elements.each( function() { 
 
             var text = $(this).html();
 
@@ -53,7 +54,7 @@ var matcher = {
                 }
                 i --;
             }
-            text = $('<p>' + text + '</p>').text();
+            text = jQuery('<p>' + text + '</p>').text();
 
 
             var results = text.match(matcher.regex);
@@ -73,7 +74,7 @@ var matcher = {
                         // Also make sure the instance we're replacing is text and not markup.
                         // THE PROBLEM IS WE'RE SEARCHING THE MARKUP NOT JUST THE TEXT AND SOMETIMES
                         // THE MARKUP CONTAINS HIDDEN ELEMENTS THAT HAVE PROPER NOUNS.
-                        console.log($(this).html().indexOf(item), $(this).html().lastIndexOf(item), text.indexOf(item), text.lastIndexOf(item), item);
+                        console.log(jQuery(this).html().indexOf(item), jQuery(this).html().lastIndexOf(item), text.indexOf(item), text.lastIndexOf(item), item);jQuery
                         // We compare the position of the item in the html vs. the text, and we also
                         // look up the last position of the item in the hmtl and the text.
                         // We do this because proper nouns are sometimes in chunk of markup more than once,
@@ -85,16 +86,16 @@ var matcher = {
                         // BUT, if first pos html != last pos html, and first pos text == last pos text, and first pos text !== first pos html,
                         // THEN WE HAVE TEXT WE WOULD BE HYPERLINKING IN AN ELEMENT'S ATTRIBUTE.
                         // When that happens, we replace the last element not the first.
-                        var firsthtml = $(this).html().indexOf(item), lasthtml = $(this).html().lastIndexOf(item), firsttext = text.indexOf(item), lasttext = text.lastIndexOf(item);
+                        var firsthtml = jQuery(this).html().indexOf(item), lasthtml = jQuery(this).html().lastIndexOf(item), firsttext = text.indexOf(item), lasttext = text.lastIndexOf(item);
                         if ( firsthtml != lasthtml && firsttext == lasttext && firsttext !== firsthtml )
                         {
                             // This regex allows us to replace against the last instance of the item.
                             // If there are more than two instances of the proper noun and the actual textual one
                             // is in the middle, we are SCREWED.
                             var pattern = new RegExp(item + "(?!.*" + item + ")");
-                            $(this).html($(this).html().replace(pattern, '<a href="' + matcher.lookup[item] + '">' + item + '</a>'));
+                            jQuery(this).html(jQuery(this).html().replace(pattern, '<a href="' + matcher.lookup[item] + '">' + item + '</a>'));
                         }
-                        else $(this).html($(this).html().replace(item, '<a href="' + matcher.lookup[item] + '">' + item + '</a>'));
+                        else jQuery(this).html(jQuery(this).html().replace(item, '<a href="' + matcher.lookup[item] + '">' + item + '</a>'));
 
                         // We only want to link the name once,
                         // so we remove it from the lookup when we're done.
@@ -114,7 +115,7 @@ var matcher = {
             this.update_config(matcher_config);
         }
 
-        $.getScript(this.config.dir + this.config.section + ".js", function()
+        jQuery.getScript(this.config.dir + this.config.section + ".js", function()
         {
             matcher.match();
         });
